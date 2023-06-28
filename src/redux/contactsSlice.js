7887fb54b5f661e5fab112d5b9f10e180228d9ee
Kirill <litovchenko.br@gmail.com>
2023-06-28 15:16:07 +0300

    hw
    fixed errors

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -16,42 +16,12 @@ const contactsSlice = createSlice({
   initialState: initialContacts,
   extraReducers: builder => {
     builder
-      //   .addCase(getContactsThunk.pending, handlePending)
       .addCase(getContactsThunk.fulfilled, handleFulfilledGet)
-      //   .addCase(getContactsThunk.rejected, handleRejected)
-      //   .addCase(addContactThunk.pending, handlePending)
       .addCase(addContactThunk.fulfilled, handleFulfilledAdd)
-      //   .addCase(addContactThunk.rejected, handleRejected)
-      //   .addCase(deleteContactThunk.pending, handlePending)
       .addCase(deleteContactThunk.fulfilled, handleFulfilledDelete)
-      //   .addCase(deleteContactThunk.rejected, handleRejected)
-      .addMatcher(
-        isAnyOf(
-          ...fn('pending')
-          //   getContactsThunk.pending,
-          //   addContactThunk.pending,
-          //   deleteContactThunk.pending
-        ),
-        handlePending
-      )
-      .addMatcher(
-        isAnyOf(
-          ...fn('rejected')
-          //   getContactsThunk.rejected,
-          //   addContactThunk.rejected,
-          //   deleteContactThunk.rejected
-        ),
-        handleRejected
-      )
-      .addMatcher(
-        isAnyOf(
-          ...fn('fulfilled')
-          //   getContactsThunk.fulfilled,
-          //   addContactThunk.fulfilled,
-          //   deleteContactThunk.fulfilled
-        ),
-        handleFulfilled
-      );
+      .addMatcher(isAnyOf(...fn('pending')), handlePending)
+      .addMatcher(isAnyOf(...fn('rejected')), handleRejected)
+      .addMatcher(isAnyOf(...fn('fulfilled')), handleFulfilled);
   },
 });
 
